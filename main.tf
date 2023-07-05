497085d0ede94f3bdcd30901eff3fdce540b93db
Rajkumar <rajkumarpatel52247@gmail.com>
2023-07-05 10:52:14 +0530

    s

---
+++ main.tf
@@ -0,0 +1,94 @@
+resource "aws_security_group" "main" {
+  name        = "${var.name}-${var.env}-sg"
+  description = "${var.name}-${var.env}-sg"
+  vpc_id = var.vpc_id
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_subnet_cidr
+  }
+
+ ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = var.sg_subnet_cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = merge({ Name = "$(var.component)-$(var.env)" }, var.tags)
+}
+
+
+resource "aws_lb" "main" {
+  name               = "${var.name}-${var.env}-lb"
+  internal           = var.internal
+  load_balancer_type = var.load_balancer_type
+  security_groups    = [aws_security_group.main.id]
+  subnets            = var.subnets
+
+  enable_deletion_protection = true
+
+
+  tags = merge({ Name = "$(var.component)-$(var.env)-lb" }, var.tags)
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = " "
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default Error"
+      status_code  = "500"
+    }
+  }
+}
+# resource "aws_lb" "test" {
+#   name               = "${var.name}-alb"
+#   internal           = false
+#   load_balancer_type = "application"
+#   security_groups    = [aws_security_group.lb_sg.id]
+#   subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+#   enable_deletion_protection = true
+
+#   access_logs {
+#     bucket  = aws_s3_bucket.lb_logs.id
+#     prefix  = "test-lb"
+#     enabled = true
+#   }
+
+#   tags = {
+#     Environment = "production"
+#   }
+# }
